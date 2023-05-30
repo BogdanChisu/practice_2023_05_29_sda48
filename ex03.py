e80943896edda7ab925a113361af568842b24c3e
BogdanChisu <bchisu@gmail.com>
2023-05-30 20:00:30 +0300

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -3,9 +3,9 @@ Creați un generator care returnează numere pare de la 0 la n, care se divid cu
 """
 
 def seven_div_gen(n):
-    for i in range(n + 1):
-        if i % 7 == 0:
-            yield i
+    for i in range(0, n+1, 14):
+        #if i % 14 == 0:
+        yield i
 
 n = int(input("Introduceti numarul n: "))
 list_seven_div = list(seven_div_gen(n))
